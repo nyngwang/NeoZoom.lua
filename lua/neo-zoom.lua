09ab243ca79116130d78b00affd26c735606d35f
Ning Wang <nyngwang@gmail.com>
2022-01-15 18:17:42 +0800

    :bug: important fix: should use tab as pivot on zoom-out

--- lua/neo-zoom.lua
+++ lua/neo-zoom.lua
@@ -35,6 +35,15 @@ local function is_a_parent(win_test)
   return {false}
 end
 
+local function is_a_child(tab_test)
+  for k, v in pairs(M.parent_info_from_win) do
+    if (tab_test == v[]) then
+      return {true, k}
+    end
+  end
+  return {false}
+end
+
 function M.maximize_current_split()
   if (vim.bo.buftype == 'nofile'
     or vim.bo.buftype == 'terminal'
@@ -47,14 +56,15 @@ function M.maximize_current_split()
     vim.api.nvim_set_current_win(is_a_parent(cur_win)[2])
     return
   end
-  -- if the current win has a parent then go back
-  if M.parent_info_from_win[cur_win] ~= nil then
+  -- if the current tab is a child, close it no matter how many splits there are
+  local cur_tab = vim.api.nvim_get_current_tabpage()
+  if is_a_child(cur_tab)[1] then
     -- as a scratch pad: the other splits discarded
     local buf_closed = vim.api.nvim_get_current_buf()
     local cur_closed = vim.api.nvim_win_get_cursor(0)
     vim.cmd('tabc')
     -- restore to the state one wants to zoom-in
-    local win_p, buf_p, cur_p = unpack(M.parent_info_from_win[cur_win])
+    local win_p, buf_p, cur_p = unpack(M.parent_info_from_win[is_a_child(cur_tab)[2]])
     -- TODO: didn't consider the case that the win_p doesn't exist anymore
     vim.api.nvim_set_current_win(win_p)
     vim.api.nvim_set_current_buf(buf_p)
