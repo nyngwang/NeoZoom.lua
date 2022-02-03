cb094fb82210525782d3de1ee958c5323fea1285
Ning Wang <nyngwang@gmail.com>
2022-02-03 19:33:14 +0800

    Fix: manually close tab error (#20)

--- lua/neo-zoom.lua
+++ lua/neo-zoom.lua
@@ -14,25 +14,43 @@ local function consume(win)
   return unpack(data)
 end
 
-local function is_a_parent(win_test)
-  local MAX = 999
-  local win_closest = nil
-  local tab_n_of_closest_win = MAX
+local function validate_mappings()
   for k, v in pairs(M.parent_info_from_win) do
-    if (win_test == v[1]) then
-      local tab_n = vim.api.nvim_tabpage_get_number(v[4])
-      if tab_n_of_closest_win > tab_n then
-        tab_n_of_closest_win = tab_n
-        win_closest = k
-      end
-    end
+    if not vim.api.nvim_win_is_valid(v[1]) then -- **parent repear**
+      consume(k) end
   end
-  if tab_n_of_closest_win == MAX then return {false}
-  else return {true, win_closest} end
+  for k, v in pairs(M.parent_info_from_win) do
+    if not vim.api.nvim_win_is_valid(k) then -- **children repear**
+      consume(k) end
+  end
+  for k, v in pairs(M.parent_info_from_win) do
+    M.parent_info_from_win[k][2] = vim.api.nvim_win_get_buf(k) -- update buffer
+  end
+end
+
+local function is_a_parent(win_test)
+  for k, v in pairs(M.parent_info_from_win) do
+    if v[1] == win_test then
+      return true end
+  end
+  return false
 end
 
 local function is_a_child(win_test)
   return M.parent_info_from_win[win_test] ~= nil
+end
+
+local function prefer_non_noname_buf(win_p)
+  for k, v in pairs(M.parent_info_from_win) do
+    if v[1] == win_p
+      and vim.api.nvim_buf_get_name(v[2]) ~= '' -- prefer non-`[No Name]`
+      then return k end
+  end
+  -- all children `[No Name]`, then go for it.
+  for k, v in pairs(M.parent_info_from_win) do
+    if v[1] == win_p
+      then return k end
+  end
 end
 
 local function clone_parent_info_to(from_win, to_win)
@@ -43,8 +61,72 @@ local function clone_parent_info_to(from_win, to_win)
     M.parent_info_from_win[from_win][1],
     M.parent_info_from_win[from_win][2],
     M.parent_info_from_win[from_win][3],
-    M.parent_info_from_win[from_win][4],
+    M.parent_info_from_win[from_win][4]
   }
+end
+
+local function pin_to_80_percent_height()
+  local scrolloff = 7
+  local cur_line = vim.fn.line('.')
+  vim.cmd("normal! zt")
+  if (cur_line > scrolloff) then
+    vim.cmd("normal! " .. scrolloff .. "k" .. scrolloff .. "j")
+  else
+    vim.cmd('normal!' .. (cur_line-1) .. 'k' .. (cur_line-1) .. 'j')
+  end
+end
+
+local function close_win_and_floats(cur_win)
+  for _, win in pairs(vim.api.nvim_list_wins()) do
+    local win_config = vim.api.nvim_win_get_config(win)
+    if win_config.relative == 'win' and win_config.win == cur_win then -- close these floats first.
+      vim.api.nvim_win_close(win, false)
+    end
+  end
+  vim.cmd('wincmd q')
+end
+---------------------------------------------------------------------------------------------------
+function M.neo_zoom()
+  if (vim.bo.buftype == 'nofile'
+    or vim.bo.buftype == 'terminal'
+    or vim.bo.filetype == 'qf') then
+    return
+  end
+  local cur_tab = vim.api.nvim_get_current_tabpage()
+  local cur_win = vim.api.nvim_get_current_win()
+  local cur_buf = vim.api.nvim_win_get_buf(0)
+  local cur_cur = vim.api.nvim_win_get_cursor(0)
+
+  validate_mappings()
+
+  if is_a_child(cur_win) then -- should close the current win and do some restore
+    local win_p = M.parent_info_from_win[cur_win][1]
+
+    vim.api.nvim_set_current_win(win_p)
+    if vim.api.nvim_win_get_buf(0) == cur_buf then -- restore cursor
+      vim.api.nvim_win_set_cursor(0, cur_cur)
+    end
+
+    -- TODO: should disable the zoom-in statusline color here
+  elseif is_a_parent(cur_win) then -- goto any child on the closest following tabs.
+    local win_c = prefer_non_noname_buf(cur_win)
+    vim.api.nvim_set_current_win(win_c)
+    if vim.api.nvim_win_get_buf(0) == cur_buf then -- restore cursor
+      vim.api.nvim_win_set_cursor(0, cur_cur)
+    end
+  else -- if the current win is neither parent nor child.
+    vim.cmd('tab split')
+    local old_win = cur_win
+    cur_win = vim.api.nvim_get_current_win()
+    M.parent_info_from_win[cur_win] = {
+      old_win,
+      vim.api.nvim_get_current_buf(),
+      vim.api.nvim_win_get_cursor(0),
+      vim.api.nvim_get_current_tabpage()
+    }
+    -- TODO: should enable the zoom-in statusline color here
+  end
+  pin_to_80_percent_height()
 end
 
 function M.neo_vsplit()
@@ -63,73 +145,6 @@ function M.neo_split()
   vim.cmd('wincmd j')
   if bottom_win == top_win then print('FUCKING IMPOSSIBLE'); return end
   clone_parent_info_to(bottom_win, top_win)
-end
-
-local function pin_to_80_percent_height()
-  local scrolloff = 7
-  local cur_line = vim.fn.line('.')
-  vim.cmd("normal! zt")
-  if (cur_line > scrolloff) then
-    vim.cmd("normal! " .. scrolloff .. "k" .. scrolloff .. "j")
-  else
-    vim.cmd('normal!' .. (cur_line-1) .. 'k' .. (cur_line-1) .. 'j')
-  end
-end
-
-local function close_win_and_floats(cur_win)
-  for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local win_config = vim.api.nvim_win_get_config(win)
-    if win_config.relative == 'win' and win_config.win == cur_win then -- close these floats first.
-      vim.api.nvim_win_close(win, false)
-    end
-  end
-  vim.cmd('wincmd q')
-end
----------------------------------------------------------------------------------------------------
-function M.neo_zoom()
-  if (vim.bo.buftype == 'nofile'
-    or vim.bo.buftype == 'terminal'
-    or vim.bo.filetype == 'qf') then
-    return
-  end
-  local cur_win = vim.api.nvim_get_current_win()
-  local cur_tab = vim.api.nvim_get_current_tabpage()
-
-  for k, v in pairs(M.parent_info_from_win) do
-    if not vim.api.nvim_win_is_valid(v[1]) then -- **parent repear**
-      consume(k) end
-  end
-
-  if is_a_child(cur_win) then -- should close the current win and do some restore
-    local win_p = consume(cur_win) -- `win_p` must be valid after **parent repear**
-    local buf_closed = vim.api.nvim_get_current_buf()
-    local cur_closed = vim.api.nvim_win_get_cursor(0)
-
-    close_win_and_floats(cur_win)
-
-    -- restore info
-    vim.api.nvim_set_current_win(win_p)
-    vim.api.nvim_set_current_buf(buf_closed)
-    vim.api.nvim_win_set_cursor(win_p, cur_closed)
-
-    -- TODO: should disable the zoom-in statusline color here
-  elseif is_a_parent(cur_win)[1] then -- go the the first child on the closest following tabs.
-    local cur_cur = vim.api.nvim_win_get_cursor(cur_win)
-    local child_win_closest = is_a_parent(cur_win)[2]
-    vim.api.nvim_set_current_win(child_win_closest)
-  else -- if the current win is neither parent nor child.
-    vim.cmd('tab split')
-    local old_win = cur_win
-    cur_win = vim.api.nvim_get_current_win()
-    M.parent_info_from_win[cur_win] = {
-      old_win,
-      vim.api.nvim_get_current_buf(),
-      vim.api.nvim_win_get_cursor(0),
-      vim.api.nvim_get_current_tabpage()
-    }
-    -- TODO: should enable the zoom-in statusline color here
-  end
-  pin_to_80_percent_height()
 end
 
 local function setup_vim_commands()
