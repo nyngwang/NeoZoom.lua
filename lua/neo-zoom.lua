8f1a982f3ee30e24f0732da1f9593d707d5daa67
nyngwang <nyngwang@gmail.com>
2022-10-05 23:24:14 +0800

    fix: should be exclude_filetype as per README.md

--- lua/neo-zoom.lua
+++ lua/neo-zoom.lua
@@ -32,17 +32,14 @@ end
 
 ---------------------------------------------------------------------------------------------------
 function M.setup(opt)
-  if opt == nil then
-    opt = {}
-  end
-  
+  if opt == nil then opt = {} end
   M.width_ratio = opt.width_ratio ~= nil and opt.width_ratio or 0.66
   M.height_ratio = opt.height_ratio ~= nil and opt.height_ratio or 0.9
   M.top_ratio = opt.top_ratio ~= nil and opt.top_ratio or 0.03
   M.left_ratio = opt.left_ratio ~= nil and opt.left_ratio or 0.32
   M.border = opt.border ~= nil and opt.border or 'double'
-  M.filetype_exclude = opt.filetype_exclude ~= nil and
-    _add_table(opt.filetype_exclude, { 'fzf', 'qf', 'dashboard' }) or { 'fzf', 'qf', 'dashboard' }
+  M.exclude_filetype = opt.exclude_filetype ~= nil and
+    _add_table(opt.exclude_filetype, { 'fzf', 'qf', 'dashboard' }) or { 'fzf', 'qf', 'dashboard' }
   M.scrolloff_on_zoom = opt.scrolloff_on_zoom ~= nil and opt.scrolloff_on_zoom or 13
 end
 
@@ -50,7 +47,7 @@ function M.neo_zoom()
   if (
       (vim.bo.buftype == 'terminal'
         and vim.api.nvim_win_get_config(0).relative == '')
-      or _in_table(M.filetype_exclude, vim.bo.filetype)
+      or _in_table(M.exclude_filetype, vim.bo.filetype)
     ) then
     return
   end
